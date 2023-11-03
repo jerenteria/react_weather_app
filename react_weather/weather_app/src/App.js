a5904ea7163aebf9c299b08c279afb22a741c31e
jerenteria <renteria.juan96@gmail.com>
2023-11-02 22:34:53 -0700

    starting new project

---
+++ react_weather/weather_app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  async function getWeather() {
+    fetch('');
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
